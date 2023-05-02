5dbb85665c2d41e048c465eb64a2fa06a6556538
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-02 16:08:43 -0300

    message ok

--- server/app.js
+++ server/app.js
@@ -6,9 +6,15 @@ var app = express();
 
 app.use(cors())
 app.use(express.json())
+
+const AuthRegisterUserRoutes = require('./routes/AuthRegisterUserRoutes')
+
 const port = process.env.PORT || 3000
+
+app.use(AuthRegisterUserRoutes)
 
 app.listen(port,()=>{
   console.log(`Servidor rodando na porta: ${port}`)
 })
+
 require("./database/connection")
