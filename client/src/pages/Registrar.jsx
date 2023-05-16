a325e4240365e3dd969eddadb9dba65423154126
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-16 15:56:55 -0300

    cadastrando usuario do jeito errado

--- client/src/pages/Registrar.jsx
+++ client/src/pages/Registrar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { useState } from 'react'
+import axios from 'axios'
 function Registrar() {
+  const [user,setUser] = useState({
+                                    name: 'testando',
+                                    email:'testando@gmail.com',
+                                    age: 12,
+                                    password: 'super123',
+                                    confirmPassword:'super123'
+                                  })
+  function cadastrarUsuario(user){
+    axios.post('http://localhost:4000/auth/register/user',user)
+      .then(res=>console.log(res.data))
+      .catch(erro=> console.log(erro))
+  }
+
   return (
-    <div>Registrar</div>
+    <div>
+      <button onClick={()=>cadastrarUsuario(user)}>Cadastrar</button>
+    </div>
   )
 }
 
