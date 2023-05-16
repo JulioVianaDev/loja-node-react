fbb12c9d530aa37a08db6e325c0d154cd33fbfe3
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-16 16:22:08 -0300

    formulario para a tarefa

--- client/src/pages/Registrar.jsx
+++ client/src/pages/Registrar.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react'
 import axios from 'axios'
 function Registrar() {
   const [user,setUser] = useState({
-                                    name: 'testando',
-                                    email:'testando@gmail.com',
-                                    age: 12,
-                                    password: 'super123',
-                                    confirmPassword:'super123'
+                                    name: '',
+                                    email:'',
+                                    age: '',
+                                    password: '',
+                                    confirmPassword:''
                                   })
   function cadastrarUsuario(user){
     axios.post('http://localhost:4000/auth/register/user',user)
@@ -17,7 +17,44 @@ function Registrar() {
 
   return (
     <div>
-      <button onClick={()=>cadastrarUsuario(user)}>Cadastrar</button>
+      <form onSubmit={()=>cadastrarUsuario(user)}>
+        <label htmlFor="nome">Nome:</label>
+        <input 
+          type="text" 
+          id="nome" 
+          value={user.name}
+          onChange={(e)=>setUser({...user,name: e.target.value})}
+        />
+        <label htmlFor="email">Email:</label>
+        <input 
+          type="text" 
+          id="email" 
+          value={user.email}
+          onChange={(e)=>setUser({...user,email: e.target.value})}
+        />
+        <label htmlFor="idade">Idade:</label>
+        <input 
+          type="number" 
+          id="idade" 
+          value={user.age}
+          onChange={(e)=>setUser({...user,age: e.target.value})}
+        />
+        <label htmlFor="senha">Senha:</label>
+        <input 
+          type="password" 
+          id="senha" 
+          value={user.password}
+          onChange={(e)=>setUser({...user,password: e.target.value})}
+        />
+        <label htmlFor="confirmPassword">Confirme Sua Senha:</label>
+        <input 
+          type="password" 
+          id="confirmPassword" 
+          value={user.confirmPassword}
+          onChange={(e)=>setUser({...user,confirmPassword: e.target.value})}
+        />
+        <button type='submit'>Cadastrar</button>
+      </form>
     </div>
   )
 }
