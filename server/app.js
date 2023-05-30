a8e1111834c85bd4d5c9efa1ae8ff2ffac2c7e52
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-30 16:30:00 -0300

    pegando os valores

--- server/app.js
+++ server/app.js
@@ -9,10 +9,13 @@ app.use(cors())
 app.use(express.json())
 
 const AuthRegisterUserRoutes = require('./routes/AuthRegisterUserRoutes')
+const ProductsRouter = require('./routes/ProductsRoutes')
 
 const port = process.env.PORT || 3000
 
 app.use(AuthRegisterUserRoutes)
+app.use('/products',ProductsRouter)
+
 app.get('/lucas', async function(req, res){
   const response = await fetch('https://api.sampleapis.com/cartoons/cartoons2D')
       const data = await response.json()
