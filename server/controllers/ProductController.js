bb90b4d1d77162cea4f16a23a679c54e655ad5d3
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-06 15:51:39 -0300

    edit ok

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -21,4 +21,21 @@ module.exports= class ProductsController{
             res.status(500).json({message: "Erro ao cadastrar o produto."})
         }
     }
+    static async editProduct(req,res){
+        try {
+            const {nome,price,desc,disponivel} = req.body;
+            const produto = await Product.findByIdAndUpdate(
+                req.params.id,
+                {nome,price,desc,disponivel} ,
+                {new: true}
+            );
+            if(!produto){
+                return res.status(404).json({message: "este produto não existe"})
+            }
+            res.json(produto);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: "Erro ao editar o produto."})
+        }
+    }
 }
--- server/routes/ProductsRoutes.js
+++ server/routes/ProductsRoutes.js
@@ -4,5 +4,7 @@ const ProductsController = require('../controllers/ProductController')
 router
     .get('/',ProductsController.getAll)
     .post('/',ProductsController.createProduct)
-    
+
+router
+    .put('/:id',ProductsController.editProduct)
 module.exports = router
