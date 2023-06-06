833bf6b1b91a1bc5ced79939f0c07bfdc330bf4f
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-06 15:30:35 -0300

    criando produto ok

--- server/routes/ProductsRoutes.js
+++ server/routes/ProductsRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const ProductsController = require('../controllers/ProductController')
 
-router.get('/',ProductsController.getAll)
-
+router
+    .get('/',ProductsController.getAll)
+    .post('/',ProductsController.createProduct)
+    
 module.exports = router
