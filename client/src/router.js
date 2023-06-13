a7dd37a3036422e16d7683052b4108c65ee638ac
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-13 16:12:19 -0300

    rota de produtos

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom"
 import Registrar from "./pages/Registrar"
 import Root from "./pages/Root"
 import ProdutosForm from "./pages/ProdutosForm"
+import Produtos from "./pages/Produtos"
 export const router = createBrowserRouter([
     {
       path: '/',
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
         {
             path:'register',
             element: <Registrar/>
+        },
+        {
+          path: 'produtos',
+          element: <Produtos/>
         }
       ]
     },
