e9ad0c8bed7946c62e9dafba55eda5a52fc3fbde
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-13 16:09:05 -0300

    product page

---
+++ client/src/pages/Produtos.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Produtos() {
+  return (
+    <div>
+        <h1> Produtos </h1>
+        <div>
+            <h2>Tomate</h2>
+            <p>Desc </p>
+            <p>Price</p>
+            <p>Disponivel</p>
+        </div>
+    </div>
+  )
+}
+
+export default Produtos
