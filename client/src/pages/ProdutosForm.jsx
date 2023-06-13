3a1a484bf9662e67027bc617771280c32049ec95
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-13 15:22:08 -0300

    checkbox efeito

--- client/src/pages/ProdutosForm.jsx
+++ client/src/pages/ProdutosForm.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function ProdutosForm() {
-  return (
-    <div>
-        <h1>Cadastre seu produto</h1>
+    const [disponivel,setDisponivel] = useState(false);
+
+    const handleCheckboxChange = (event)=>{
+        setDisponivel(event.target.checked);
+    }
+
+    return (
         <div>
-            <form>
-                <label htmlFor="nome">Nome</label>
-                <input type="text" id="nome" name="nome" />
-                <label htmlFor="desc">desc</label>
-                <input type="text" id="desc" name="desc" />
-                <label htmlFor="price">price</label>
-                <input type="number" id="price" name="price" />
-                
-            </form>
+            <h1>Cadastre seu produto</h1>
+            <div>
+                <form>
+                    <label htmlFor="nome">Nome</label>
+                    <input type="text" id="nome" name="nome" />
+                    <label htmlFor="desc">desc</label>
+                    <input type="text" id="desc" name="desc" />
+                    <label htmlFor="price">price</label>
+                    <input type="number" id="price" name="price" />
+                    <label htmlFor="disponivel"> Produto Disponivel:</label>
+                    <input 
+                        type="checkbox" 
+                        name="disponivel" 
+                        id="disponivel" 
+                    />
+                    <label htmlFor="nDisponivel"> Produto Não Disponivel:</label>
+                    <input 
+                        type="checkbox" 
+                        name="nDisponivel" 
+                        id="nDisponivel" 
+                    />
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProdutosForm
