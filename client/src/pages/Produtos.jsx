908f691797b37ebe4261b773808283ce2d78e569
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-13 16:21:54 -0300

    consumindo a api

--- client/src/pages/Produtos.jsx
+++ client/src/pages/Produtos.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 function Produtos() {
+    const [produtos,setProdutos] = useState([])
+    useEffect(()=>{
+        axios.get('http://localhost:4000/products')
+            .then(res=>setProdutos(res.data))
+            .catch(err=>console.log(err))
+    },[])
   return (
     <div>
         <h1> Produtos </h1>
-        <div>
-            <h2>Tomate</h2>
-            <p>Desc </p>
-            <p>Price</p>
-            <p>Disponivel</p>
-        </div>
+        
+        {
+            produtos.map(p=> <div>
+                                <h2>Tomate</h2>
+                                <p>Desc </p>
+                                <p>Price</p>
+                                <p>Disponivel</p>
+                            </div>
+            )
+        }
+        
     </div>
   )
 }
