be695239cbb42394d59a0a099aaf597674f9c6ea
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-13 15:14:29 -0300

    form 80%

--- client/src/pages/ProdutosForm.jsx
+++ client/src/pages/ProdutosForm.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 function ProdutosForm() {
   return (
     <div>
-        
+        <h1>Cadastre seu produto</h1>
+        <div>
+            <form>
+                <label htmlFor="nome">Nome</label>
+                <input type="text" id="nome" name="nome" />
+                <label htmlFor="desc">desc</label>
+                <input type="text" id="desc" name="desc" />
+                <label htmlFor="price">price</label>
+                <input type="number" id="price" name="price" />
+                
+            </form>
+        </div>
     </div>
   )
 }
